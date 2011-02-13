2a065baf0dc0ae7e976827217f116ceb9af50644
jamsea <jhush91@gmail.com>
2011-02-12 19:31:52 -0500

    Added help page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,6 +10,13 @@ describe PagesController do
     end
   end
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+  
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
