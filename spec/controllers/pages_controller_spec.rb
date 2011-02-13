cf62d7bf4a856aec5e7cee1a0914979f1f75c2a2
jamsea <jhush91@gmail.com>
2011-02-12 19:40:00 -0500

    Modified test cases with @base_title variable

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,24 +3,33 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"  
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-  end
-
-  describe "GET 'help'" do
-    it "should be successful" do
+    
+    it "should have the right title" do
       get 'home'
-      response.should be_success
+      response.should have_selector("title",
+                                    :content => @base_title + " | Home")
     end
   end
-  
+
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Contact")
     end
   end
 
@@ -29,13 +38,11 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => @base_title + " | About")
+    end
   end
-
-
-  it "should have the right title" do
-    get 'home'
-    response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Home")
-  end
-
 end
