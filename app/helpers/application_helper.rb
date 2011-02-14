2c0d71c412569cad9932f4bf1a0af78c23b2f6a8
jamsea <jhush91@gmail.com>
2011-02-14 04:20:59 -0500

    Changed layout, got everything to work. Thank God

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   
   # Return a title on a per-page basis.
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Jamie Hush  Essex Energy Web Application Test"
     if @title.nil?
       base_title
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,27 @@
 # == Schema Information
-# Schema version: 20110213223842
+# Schema version: 20110214084141
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean
+#  role               :string(255)
 #
 
 class User < ActiveRecord::Base
   attr_accessor   :password
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :role
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+
+  validates :role,  :presence => true,
+                    :length   => { :maximum => 50 }  
   validates :name,  :presence => true,
                     :length   => { :maximum => 50 }
   validates :email, :presence   => true,
